92b7dc1b95c472ea340d675d508cb2559a656a9f
RunzeShi <shisun@sheridancollege.ca>
2026-03-28 18:40:05 -0400

    Initialize Assignment 3 Spring Boot project structure

---
+++ src/main/java/sheridan/runze/assignment3/LibraryDataServerApplication.java
@@ -0,0 +1,13 @@
+package sheridan.runze.assignment3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryDataServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/runze/assignment3/LibraryDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.runze.assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
