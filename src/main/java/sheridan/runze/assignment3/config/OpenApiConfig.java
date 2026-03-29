15bdb848524f79dca76fdddbfbd75c6622019342
RunzeShi <shisun@sheridancollege.ca>
2026-03-28 20:55:31 -0400

    Add OpenAPI documentation configuration

---
+++ src/main/java/sheridan/runze/assignment3/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package sheridan.runze.assignment3.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI libraryDataOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Data API")
+                        .description("REST API for library book data")
+                        .version("1.0"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("PROG27545 Assignment 3"));
+    }
+}
